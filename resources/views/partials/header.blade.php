0b4bfca550fe42bab21451acbb2a96c84452c483
Danielateresa <dany.t_87@hotmail.it>
2022-12-21 17:15:38 +0100

    Fixed link on header

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -1,7 +1,7 @@
 <header>
     <nav class="nav justify-content-center">
-        <a class="nav-link " href="#" aria-current="page">Home</a>
-        <a class="nav-link" href="#">Link</a>
-        <a class="nav-link " href="#">link</a>
+        <a class="nav-link " href="{{'/'}}" aria-current="page">Comics Database</a>
+        <a class="nav-link disabled" href="#">Link</a>
+        <a class="nav-link disabled" href="#">link</a>
     </nav>
 </header>
