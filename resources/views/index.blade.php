8425c7248b008ed0b5f9a2b6fc97335887cbfb71
Danielateresa <dany.t_87@hotmail.it>
2022-12-22 18:58:39 +0100

    Added modal (bonus), modiied buttons

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -40,16 +40,45 @@
                     <td>{{$comic->sale_date}}</td>
                     <td>{{$comic->type}}</td>
                     <td>
-                        <a href="{{route('show',$comic->id)}}"><button type="submit" class="btn btn-prymary"><i
-                                    class="fa-regular fa-eye px-1"></i></button></a>
-                        <a href="{{route('edit',$comic->id)}}"><button type="submit" class="btn btn-prymary"><i
-                                    class="fa-regular fa-pen-to-square px-1"></i></button></a>
-                        <form action="{{route('destroy',$comic->id)}}" method="post">
-                            @csrf
-                            @method('DELETE')
-                            <button type="submit" class="btn btn-warning"><i
-                                    class="fa-solid fa-trash px-1"></i></button>
-                        </form>
+                        <a href="{{route('show',$comic->id)}}" class="btn btn-secondary"><i
+                                class="fa-regular fa-eye"></i></a>
+                        <a href="{{route('edit',$comic->id)}}" class="btn btn-primary my-1"><i
+                                class="fa-regular fa-pen-to-square"></i></a>
+
+                        <!-- Modal trigger button -->
+                        <button type="button" class="btn btn-warning" data-bs-toggle="modal"
+                            data-bs-target="#deleteComic-{{$comic->id}}">
+                            <i class="fa-solid fa-trash text-white"></i>
+                        </button>
+
+                        <!-- Modal Body -->
+                        <!-- if you want to close by clicking outside the modal, delete the last endpoint:data-bs-backdrop and data-bs-keyboard -->
+                        <div class="modal fade" id="deleteComic-{{$comic->id}}" tabindex="-1" data-bs-backdrop="static"
+                            data-bs-keyboard="false" role="dialog" aria-labelledby="modalComicId-{{$comic->id}}"
+                            aria-hidden="true">
+                            <div class="modal-dialog modal-dialog-scrollable modal-dialog-centered modal-sm"
+                                role="document">
+                                <div class="modal-content">
+                                    <div class="modal-header">
+                                        <h5 class="modal-title" id="modalComicId-{{$comic->id}}">Delete Comic</h5>
+                                        <button type="button" class="btn-close" data-bs-dismiss="modal"
+                                            aria-label="Close"></button>
+                                    </div>
+                                    <div class="modal-body">
+                                        Do you really want to delete this element permanently?
+                                    </div>
+                                    <div class="modal-footer">
+                                        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">No,
+                                            close</button>
+                                        <form action="{{route('destroy',$comic->id)}}" method="post">
+                                            @csrf
+                                            @method('DELETE')
+                                            <button type="submit" class="btn btn-warning">Yes, delete</button>
+                                        </form>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
 
                     </td>
                 </tr>
