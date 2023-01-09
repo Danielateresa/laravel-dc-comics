817f5f5f1731879e267464eebda74fc09e418531
Danielateresa <dany.t_87@hotmail.it>
2023-01-09 16:51:33 +0100

    Fixed 'description' and 'date' rules and messages

--- app/Http/Requests/UpdateComicRequest.php
+++ app/Http/Requests/UpdateComicRequest.php
@@ -25,11 +25,11 @@ class UpdateComicRequest extends FormRequest
     {
         return [
                 'title'=>'required|min:3',
-                'description'=>'nullable|max:255',
+                'description'=>'nullable',
                 'thumb'=>'nullable|max:255',
                 'price'=>'nullable|decimal:2',
                 'series'=>'nullable|max:255',
-                'sale_date'=>'nullable|date_format:Y/m/d',
+                'sale_date'=>'nullable|date_format:Y-m-d',
                 'type'=>'nullable|max:255',
         ];
     }
@@ -38,10 +38,9 @@ class UpdateComicRequest extends FormRequest
         return[
             'title.required' => 'Il campo Title deve essere compilato',
             'title.min' => 'Il campo Title deve essere lungo almeno :min caratteri',
-            'description.max' => 'Il campo Description non può contenere più di :max caratteri',
             'thumb.max' => 'Il campo Thumb non può contenere più di :max caratteri',
             'price.decimal' => 'Il campo Price deve contenere un prezzo con 2 numeri decimali',
-            'sale_date.date_format' => 'Il campo Sale date deve essere formattato come Y/m/d',
+            'sale_date.date_format' => 'Il campo Sale date deve essere formattato come Y-m-d',
             'type.max' => 'Il campo Type non può contenere più di :max caratteri',
         ];
     }
