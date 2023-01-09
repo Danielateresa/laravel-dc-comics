36be784c43a5d50accea01ad876482668cf445ea
Danielateresa <dany.t_87@hotmail.it>
2023-01-09 16:39:42 +0100

    Added active status class to page route

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -1,7 +1,9 @@
 <header>
     <nav class="nav justify-content-center">
-        <a class="nav-link " href="{{route('index')}}" aria-current="page">Comics Database</a>
-        <a class="nav-link " href="{{route('create')}}">Add New Comic</a>
+        <a class="nav-link {{Route::currentRouteName() === 'index' ? 'active' : ''}}" href="{{route('index')}}"
+            aria-current="page">Comics Database</a>
+        <a class="nav-link {{Route::currentRouteName() === 'create' ? 'active' : ''}}" href="{{route('create')}}"
+            href="{{route('create')}}">Add New Comic</a>
         <a class="nav-link disabled" href="#">link</a>
     </nav>
 </header>
